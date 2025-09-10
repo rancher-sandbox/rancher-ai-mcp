a7e4abbec61e6c66d3b51ea80dfc70fa2246f707
raul <raul.cabello@suse.com>
2025-09-10 16:25:21 +0200

    wip

--- internal/tools/tools.go
+++ internal/tools/tools.go
@@ -1,18 +1,35 @@
 package tools
 
 import (
+	"bytes"
 	"context"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"github.com/modelcontextprotocol/go-sdk/mcp"
 	"io"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/tools/clientcmd"
 	"net/http"
+	"regexp"
 	"strings"
+
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 )
 
 const (
 	steveEndpoint = "v1"
 	tokenHeader   = "R_token"
 	urlHeader     = "R_url"
+	firstLen      = 49
+	certDelim     = "\\\n      "
+)
+
+var (
+	splitRegexp = regexp.MustCompile(`\S{1,76}`)
 )
 
 // TODO add missing resources
@@ -48,6 +65,13 @@ type GetKubernetesResourceParams struct {
 	Kind      string `json:"kind" jsonschema:"the kind of the resource"`
 }
 
+type UpdateKubernetesResourceParams struct {
+	Name      string        `json:"name" jsonschema:"the name of k8s resource"`
+	Namespace string        `json:"namespace" jsonschema:"the namespace of the resource"`
+	Kind      string        `json:"kind" jsonschema:"the kind of the resource"`
+	Patch     []interface{} `json:"patch" jsonschema:"the patch of the request"`
+}
+
 type GetNonNamespacedResourceParams struct {
 	Name string `json:"name" jsonschema:"the name of k8s resource"`
 	Kind string `json:"kind" jsonschema:"the kind of the resource"`
@@ -75,6 +99,35 @@ func GetNamespacedKubernetesResource(_ context.Context, toolReq *mcp.CallToolReq
 
 	return &mcp.CallToolResult{
 		Content: []mcp.Content{&mcp.TextContent{Text: resp}},
+	}, nil, nil
+}
+
+func UpdateNamespacedKubernetesResource(ctx context.Context, toolReq *mcp.CallToolRequest, params UpdateKubernetesResourceParams) (*mcp.CallToolResult, any, error) {
+	rancherURL := toolReq.Extra.Header.Get(urlHeader)
+	clusterURL := rancherURL + "/k8s/clusters/local"
+	dynClient, err := createDynamicClient(toolReq.Extra.Header.Get(tokenHeader), clusterURL)
+	gvr := schema.GroupVersionResource{
+		Group:    "apps",
+		Version:  "v1",
+		Resource: "deployments",
+	}
+	patchBytes, err := json.Marshal(params.Patch)
+	if err != nil {
+		return nil, nil, err
+	}
+	obj, err := dynClient.Resource(gvr).Namespace(params.Namespace).Patch(ctx, params.Name, types.JSONPatchType, patchBytes, metav1.PatchOptions{})
+	if err != nil {
+		return nil, nil, err
+	}
+	fmt.Println(obj)
+
+	jsonBytes, err := json.Marshal(obj)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return &mcp.CallToolResult{
+		Content: []mcp.Content{&mcp.TextContent{Text: string(jsonBytes)}},
 	}, nil, nil
 }
 
@@ -135,3 +188,116 @@ func doRequest(url string, token string) (string, error) {
 
 	return string(body), nil
 }
+
+func formatCertString(certData string) string {
+	buf := &bytes.Buffer{}
+	if len(certData) > firstLen {
+		buf.WriteString(certData[:firstLen])
+		certData = certData[firstLen:]
+	} else {
+		return certData
+	}
+
+	for _, part := range splitRegexp.FindAllStringSubmatch(certData, -1) {
+		buf.WriteString(certDelim)
+		buf.WriteString(part[0])
+	}
+
+	return buf.String()
+}
+
+func caCertString() string {
+	certData := `-----BEGIN CERTIFICATE-----
+  MIIBvDCCAWOgAwIBAgIBADAKBggqhkjOPQQDAjBGMRwwGgYDVQQKExNkeW5hbWlj
+  bGlzdGVuZXItb3JnMSYwJAYDVQQDDB1keW5hbWljbGlzdGVuZXItY2FAMTc1NzMy
+  NTYwMTAeFw0yNTA5MDgxMDAwMDFaFw0zNTA5MDYxMDAwMDFaMEYxHDAaBgNVBAoT
+  E2R5bmFtaWNsaXN0ZW5lci1vcmcxJjAkBgNVBAMMHWR5bmFtaWNsaXN0ZW5lci1j
+  YUAxNzU3MzI1NjAxMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEl7ynOU4cwm2N
+  9/MQB5To5PKx+ZE+LeKUc0ftt/KvVJa4NIMlNqsNPEO1GQ3UCGprF9dEnK1a34JB
+  fatwpnZA26NCMEAwDgYDVR0PAQH/BAQDAgKkMA8GA1UdEwEB/wQFMAMBAf8wHQYD
+  VR0OBBYEFB3an3o9DMrBJtHD6oiBVFVvdOIuMAoGCCqGSM49BAMCA0cAMEQCIFoi
+  XFAZ+YHTwNR6FN+t9CMK9jkBFDLOAvDsZJD+GrjBAiAfUMOwdx7Z3eX2hJjlQPej
+  X5jf0OdLHZqOTUAV/zipFw==
+  -----END CERTIFICATE-----`
+	if certData == "" {
+		return ""
+	}
+	certData = base64.StdEncoding.EncodeToString([]byte(certData))
+	return formatCertString(certData)
+}
+
+func createDynamicClient(token string, url string) (*dynamic.DynamicClient, error) {
+	kubeconfig := clientcmdapi.NewConfig()
+
+	kubeconfig.Clusters["cluster"] = &clientcmdapi.Cluster{
+		Server: url,
+		//CertificateAuthorityData: []byte(caCertString()),
+		InsecureSkipTLSVerify: true,
+	}
+
+	kubeconfig.AuthInfos["mcp"] = &clientcmdapi.AuthInfo{
+		Token: token,
+	}
+
+	kubeconfig.Contexts["cluster"] = &clientcmdapi.Context{
+		Cluster:  "cluster",
+		AuthInfo: "mcp",
+	}
+
+	kubeconfig.CurrentContext = "cluster"
+
+	restConfig, err := clientcmd.NewNonInteractiveClientConfig(
+		*kubeconfig,
+		kubeconfig.CurrentContext,
+		&clientcmd.ConfigOverrides{},
+		nil,
+	).ClientConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	dynClient, err := dynamic.NewForConfig(restConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return dynClient, nil
+}
+
+/*
+func getCAFromSettings() string {
+	kubeconfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		clientcmd.NewDefaultClientConfigLoadingRules(),
+		&clientcmd.ConfigOverrides{},
+	)
+	restConfig, err := kubeconfig.ClientConfig()
+	if err != nil {
+		log.Fatalf("failed to get kubeconfig: %v", err)
+	}
+
+	// Create a dynamic client
+	dynClient, err := dynamic.NewForConfig(restConfig)
+	if err != nil {
+		log.Fatalf("failed to create dynamic client: %v", err)
+	}
+
+	// Define the GroupVersionResource for Rancher settings
+	gvr := schema.GroupVersionResource{
+		Group:    "management.cattle.io",
+		Version:  "v3",
+		Resource: "settings", // plural of the CRD
+	}
+
+	// Fetch the resource named "aaa"
+	setting, err := dynClient.Resource(gvr).Get(context.TODO(), "aaa", metav1.GetOptions{})
+	if err != nil {
+		log.Fatalf("failed to get setting: %v", err)
+	}
+
+	// Print the unstructured object
+	fmt.Printf("Got setting: %v\n", setting)
+
+	return ""
+
+}
+*/
--- main.go
+++ main.go
@@ -10,23 +10,36 @@ import (
 
 func main() {
 	server := mcp.NewServer(&mcp.Implementation{Name: "pod finder", Version: "v1.0.0"}, nil)
-	mcp.AddTool(server, &mcp.Tool{
-		Name:        "getKubernetesResource",
-		Description: "Returns non-namespaced kubernetes resource. Resources supported: User, Cluster, ClusterRole, ClusterRoleBinding, PersistentVolume"},
-		tools.GetNonNamespacedKubernetesResource)
+	/*mcp.AddTool(server, &mcp.Tool{
+	Name:        "getKubernetesResource",
+	Description: "Returns non-namespaced kubernetes resource. Resources supported: User, Cluster, ClusterRole, ClusterRoleBinding, PersistentVolume"},
+	tools.GetNonNamespacedKubernetesResource)*/
 	mcp.AddTool(server, &mcp.Tool{
 		Name:        "getKubernetesNamespacedResource",
 		Description: "Returns namespaced kubernetes resource. Resources supported: Pod, Service, ConfigMap, Secret, Deployment, StatefulSet, DaemonSet, ReplicaSet, Ingress, NetworkPolicy, HorizontalPodAutoscaler, ServiceAccount, Role, RoleBinding, PersistentVolumeClaim, Project, Bundle, GitRepo"},
 		tools.GetNamespacedKubernetesResource)
+	/*mcp.AddTool(server, &mcp.Tool{
+	Name:        "getKubernetesNamespacedResourceList",
+	Description: "Returns a list of kubernetes resources inside a namespace. Resources supported: Pod, Service, ConfigMap, Secret, Deployment, StatefulSet, DaemonSet, ReplicaSet, Ingress, NetworkPolicy, HorizontalPodAutoscaler, ServiceAccount, Role, RoleBinding, PersistentVolumeClaim, Project, Bundle, GitRepo"},
+	tools.GetNamespacedKubernetesResourceList)*/
 	mcp.AddTool(server, &mcp.Tool{
-		Name:        "getKubernetesNamespacedResourceList",
-		Description: "Returns a list of kubernetes resources inside a namespace. Resources supported: Pod, Service, ConfigMap, Secret, Deployment, StatefulSet, DaemonSet, ReplicaSet, Ingress, NetworkPolicy, HorizontalPodAutoscaler, ServiceAccount, Role, RoleBinding, PersistentVolumeClaim, Project, Bundle, GitRepo"},
-		tools.GetNamespacedKubernetesResourceList)
+		Name: "patchKubernetesNamespacedResource",
+		Description: `Description: Patches a namespaced Kubernetes resource using a JSON patch. The JSON patch must be provided as a string.
+Parameters:
+resource_type (string): The type of Kubernetes resource to patch (e.g., Pod, Deployment, Service).
+namespace (string): The namespace where the resource is located.
+resource_name (string): The name of the specific resource to patch.
+patch (json): Patch to apply. This must be a JSON object. The content type used is application/json-patch+json.
+Returns the modified resource.
+
+Example of the patch parameter:
+[{\"op\": \"replace\", \"path\": \"/spec/replicas\", \"value\": 3}]`},
+		tools.UpdateNamespacedKubernetesResource)
 
 	handler := mcp.NewStreamableHTTPHandler(func(request *http.Request) *mcp.Server {
 		return server
 	}, &mcp.StreamableHTTPOptions{})
 
-	log.Println("Listening on :9090")
-	log.Fatal(http.ListenAndServe(":9090", handler))
+	log.Println("Listening on :9092")
+	log.Fatal(http.ListenAndServe(":9092", handler))
 }
