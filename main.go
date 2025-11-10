8711377846c5b293e5c5f01a3f6a4eb8c4215213
raul <raul.cabello@suse.com>
2025-11-10 16:04:32 +0100

    add tls config
    Signed-off-by: Raul Cabello Martin <raul.cabello@suse.com>
    Signed-off-by: raul <raul.cabello@suse.com>

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"log"
 	"net/http"
 	"os"
@@ -140,6 +141,18 @@ func main() {
 					tlsName,
 				},
 				FilterCN: dynamiclistener.OnlyAllow(tlsName),
+				TLSConfig: &tls.Config{
+					MinVersion: tls.VersionTLS12,
+					CipherSuites: []uint16{
+						tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+						tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+						tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+						tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+						tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+						tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+					},
+					ClientAuth: tls.RequestClientCert,
+				},
 			},
 		})
 		if err != nil {
